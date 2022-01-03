ec83fc02a9bcafcb137a446d512995513d2f1a0e
gaponukz <49754258+gaponukz@users.noreply.github.com>
2022-01-03 14:39:31 +0200

    Update filechecker.py

--- filechecker.py
+++ filechecker.py
@@ -1,5 +1,6 @@
 import os
 import time
+import psutil
 
 from typing import Callable
 
@@ -10,6 +11,17 @@ def _read_file_contents(path: str):
     with open(path, 'r', encoding='utf-8') as out:
         return out.read()
 
+def _get_processes() -> list:
+    all_processes = []
+
+    for proc in psutil.process_iter():
+        try:
+            all_processes.append(proc.name())
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    
+    return all_processes
+
 def on_directory_changed(**kwargs: dict):
     def warpper(function: Callable):
         ''' function(status: str, items: list) '''
@@ -19,7 +31,8 @@ def on_directory_changed(**kwargs: dict):
         while True:
             if current_items != (new := _current_directory_items(path)):
                 items = list(set(current_items) ^ set(new))
-                status = "CREATE" if len(current_items) < len(new) else "DELETE" if len(current_items) > len(new) else "REPLACE"
+                status = "CREATE" if len(current_items) < len(new) else \
+                    "DELETE" if len(current_items) > len(new) else "REPLACE"
 
                 function(status, items)
             
@@ -31,6 +44,7 @@ def on_directory_changed(**kwargs: dict):
 
 def on_file_changed(**kwargs: dict):
     def warpper(function: Callable):
+        ''' function() '''
         if not (filename := kwargs.get('filename')):
             raise ValueError("filename must be specified")
         
@@ -41,6 +55,25 @@ def on_file_changed(**kwargs: dict):
                 function()
             
             current_content = new
+
+            time.sleep(kwargs.get('time', 1))
+    
+    return warpper
+
+def on_progress_changed(**kwargs: dict):
+    def warpper(function: Callable):
+        ''' function(status: str, items: list) '''
+        current_processes = _get_processes()
+
+        while True:
+            if current_processes != (new := _get_processes()):
+                items = list(set(current_processes) ^ set(new))
+                status = "CREATE" if len(current_processes) < len(new) else \
+                    "DELETE" if len(current_processes) > len(new) else "REPLACE"
+
+                function(status, items)
+            
+            current_processes = new
 
             time.sleep(kwargs.get('time', 1))
     
