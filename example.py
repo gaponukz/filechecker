476966d906400459bf2a2e1e26fa91641ddb0672
gaponukz <49754258+gaponukz@users.noreply.github.com>
2022-01-03 02:53:44 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,18 @@
+from filechecker import (
+    on_directory_changed,
+    on_file_changed
+)
+
+@on_file_changed(filename="test.txt")
+def inside_function():
+    print("Change!!!")
+
+@on_directory_changed()
+def inside_function(status: str, items: list):
+    match status:
+        case 'CREATE':
+            print(f"We found created file: {items}")
+        case 'DELETE':
+            print(f"Oh on, file {items} was deleted")
+        case 'REPLACE':
+            print("You have problems...")
