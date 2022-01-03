552489d396f941c37899fb8e13dc96182cd4092f
gaponukz <49754258+gaponukz@users.noreply.github.com>
2022-01-03 14:39:58 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 from filechecker import (
     on_directory_changed,
-    on_file_changed
+    on_file_changed,
+    on_progress_changed
 )
 
 @on_file_changed(filename="test.txt")
@@ -14,5 +15,15 @@ def inside_function(status: str, items: list):
             print(f"We found created file: {items}")
         case 'DELETE':
             print(f"Oh on, file {items} was deleted")
+        case 'REPLACE':
+            print("You have problems...")
+
+@on_progress_changed()
+def inside_function(status: str, items: list):
+    match status:
+        case 'CREATE':
+            print(f"We found created process: {items}")
+        case 'DELETE':
+            print(f"Oh on, process {items} was deleted")
         case 'REPLACE':
             print("You have problems...")
